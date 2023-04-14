8abfde4d15962374d36f27115cfa7d7eb88aa1ca
thanhgiangsgu <93782548+thanhgiangsgu@users.noreply.github.com>
2023-04-14 10:15:46 +0700

    Add files via upload

--- client/src/component/ShowAnswer/index.js
+++ client/src/component/ShowAnswer/index.js
@@ -3,6 +3,7 @@ import { Space } from 'antd'
 import './ShowAnswer.css'
 
 const ShowAnswer = ({ phase, listAnswer, question }) => {
+    console.log(listAnswer);
     return phase === 1 ? (
         <div className='container'>
             <div className='box-title'>
@@ -12,13 +13,19 @@ const ShowAnswer = ({ phase, listAnswer, question }) => {
                 <br></br>
                 <br></br>
             </div>
-            <h1 style={{ color: 'green' }}>Câu trả lời : {listAnswer.data}</h1>
+            <h1 style={{ color: 'green' }}>Câu trả lời : {listAnswer.data.correctAnswers.content}</h1>
         </div>
     ) : (
         <div className='container'>
+            <h2 style={{ fontSize: '45px' }}>{listAnswer.data.question.name}</h2>
+            <br></br>
+            <h4 style={{ color: 'crimson', fontSize: '30px', width: '80%' }}>{listAnswer.data.question.content}</h4>
+            
+            <h2 style={{ color: 'green', marginTop: '15px' }}>Câu trả lời : {listAnswer.data.question.correctAnswers.content}</h2>
             <h1 style={{ fontSize: '50px', fontWeight: '900', color: 'rebeccapurple', textShadow: '1px 1px' }}>Danh sách các câu trả lời</h1>
             <div className='box-title'>
-                {listAnswer.data.map((team) => {
+
+                {listAnswer.data.teamAnswerList.map((team) => {
                     return (
                         <Space direction='horizontal' className='list-answer'
                             style={{ width: '1000px', display: "flex", justifyContent: 'space-between' }}>
